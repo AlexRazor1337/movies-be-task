44bbc7cb3dd42b4b74fed3b54db8b2aff2d44765
AlexRazor <alexrazor1337@gmail.com>
2023-04-14 13:21:02 +0300

    Initial setup

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # movies-be-task
+
+Build Docker image:
+```sh
+docker build . -t alexrazor/movies
+```
+
+Run app using Docker:
+```sh
+docker run --name movies -p 8000:8050 -e APP_PORT=8050 alexrazor/movies
+```
---
+++ index.js
@@ -0,0 +1,6 @@
+const app = require('./app');
+
+const port = process.env.APP_PORT;
+if (!port) throw new Error('APP_PORT enviroment variable is not set!');
+
+app.listen(port, () => console.log(`Started server on port ${port}!`));
